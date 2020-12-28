ab024696493fd329f09cbcf674821f284c178b26
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2020-12-28 17:22:05 -0600

    video 18 de Dorian
    video 18 de Dorian tema: FUNCIONES

---
+++ js/video18DorianFunciones.js
@@ -0,0 +1,33 @@
+
+/*
+const saludarUsuario = (pUser)=>{
+
+	console.log(`hola ${pUser}`);
+
+}
+
+saludarUsuario('luis');
+
+saludarUsuario('pedro');
+
+*/
+
+
+//Una forma de escribirla
+
+/*
+const sumar=(pNum1, pNum2)=>{
+	return pNum1+pNum2;
+}
+
+*/
+
+//cuando es una linea se puede compactar
+
+const sumar = (pNum1,pNum2) => {
+	console.log(`la suma es: ${pNum1+pNum2}`);
+}
+
+sumar(3,9);
+
+
