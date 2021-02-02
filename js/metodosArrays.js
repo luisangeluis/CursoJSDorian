46b91b573c871f02bfd234df8fb082dd04cae8b9
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-02-02 14:01:44 -0600

    Se revisa el video 21 metodos del array
    Se revisa el vídeo 21 metodos del array de Dorian

--- js/metodosArrays.js
+++ js/metodosArrays.js
@@ -14,13 +14,64 @@ console.log(numbers.sort((a,b)=>a-b));
 
 console.log(numbers.sort((a,b)=>b-a));
 */
-
+/*
 const numbers = [11,44,1,0,48];
-
+*/
 
 //recorre el array 
 //numbers.forEach((number)=>console.log(number));
 
+/*
 numbers.forEach((number, index)=>
-	console.log(`${number} esta en la posicion ${index}`));
 
+	console.log(`${number} esta en la posicion ${index}`));
+*/
+/*
+const words =['HTML', 'CCS', 'JavaScript', 'PHP']
+
+console.log(words.some(word => word.length>9));	
+console.log(words.every(word => word.length>4));	
+*/
+
+//const numbers = [1,2,3,4,5];
+//map le aplica las instrucciones a cada uno de los elementos
+/*
+const numbersPor2 = numbers.map(number => number*2);
+console.log(numbersPor2);
+*/
+//busca que elementos cumplen la condicion y devuelve un nuevo array
+
+/*
+const numbersPor2 = numbers.filter(number => number>40);
+console.log(numbersPor2);
+
+*/
+
+//console.log(numbers.reduce((a,b)=>a*b));
+
+const users =[
+	{
+		name: 'user1',
+		online: true
+	},
+	{
+		name: 'user2',
+		online: false
+	},
+	{
+		name: 'user3',
+		online: false
+	},
+	{
+		name: 'user4',
+		online: true
+	}
+]
+
+const userOnline= users.reduce((cont,user)=>{
+	if(user.online)
+		cont++;
+	return cont;
+},0)
+
+console.log(`hay ${userOnline} usuarios conectados`);
