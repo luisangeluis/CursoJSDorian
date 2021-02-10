0da4e61b1f90447f2690f31c1bcc89ffe2d8dace
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-02-10 14:36:54 -0600

    video 24 del curso dorian pendiente

---
+++ js/seleccionarElementosDom.js
@@ -0,0 +1,14 @@
+const title = document.getElementById('title');
+//const paragraph = document.querySelector('.paragraph:nth-child(3)');
+
+
+const paragraph = document.querySelector('.paragraph');
+
+const span = paragraph.querySelector('span');
+
+title.textContent = 'accediendo a nodos texto en el titulo';
+
+//console.log(paragraph);
+console.log(span.textContent);
+
+
