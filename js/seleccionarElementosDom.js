33496924923773a240bafec0f076e813212dae9a
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-02-12 17:06:08 -0600

    video 24 del curso terminado
    vídeo 24 del curso terminado seleccionar elementos del dom

--- js/seleccionarElementosDom.js
+++ js/seleccionarElementosDom.js
@@ -1,3 +1,4 @@
+/*
 const title = document.getElementById('title');
 //const paragraph = document.querySelector('.paragraph:nth-child(3)');
 
@@ -11,4 +12,18 @@ title.textContent = 'accediendo a nodos texto en el titulo';
 //console.log(paragraph);
 console.log(span.textContent);
 
+*/
+
+const paragraph = document.querySelectorAll('.paragraph');
+
+
+//HACE QUE EL NODE LIST SE COMPORTE COMO UN ARRAY
+const paragraphArray = Array.from(document.querySelectorAll('.paragraph'));
+
+
+
+//A LO QUE ENTIENDO ITERA CON CADA ELEMENTO
+ paragraphArray.map(p=>p.style.color='blue');
+
+console.log(paragraph);
 
