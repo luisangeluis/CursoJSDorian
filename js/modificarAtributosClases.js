9eca800874d3d63d28405797a3b3aa1e6e1d993c
luisangeluis <luisangeluis@gmail.com>
2021-02-15 22:56:01 -0600

    Se revisa el video 25 modificar class y attrib terminado

---
+++ js/modificarAtributosClases.js
@@ -0,0 +1,21 @@
+const title = document.getElementById('title');
+const name = document.getElementById('nombre');
+
+//name.setAttribute('type','date');
+//console.log(name.getAttribute('type'));
+
+//title.classList.add('main-title');
+//title.classList.remove('title');
+/*
+if(title.classList.contains('title'))
+console.log('title tiene la clase title');
+else
+console.log('title no tiene la clase title');
+*/
+
+//title.classList.replace('title','otra-clase');
+/*
+console.log(title);
+console.log(name);
+*/
+console.log(name.value)
