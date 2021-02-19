d932bafc19dd20cb548b219e0e970d7649458369
luisangeluis <luisangeluis@gmail.com>
2021-02-18 18:03:36 -0600

    Se revisan los eventos de mouse video 26 terminado

---
+++ js/eventosRaton1.js
@@ -0,0 +1,28 @@
+const button = document.getElementById('button');
+const div = document.getElementById('box');
+
+// button.addEventListener('click',()=>{
+//     console.log('click')
+// });
+
+// button.addEventListener('dblclick',()=>{
+//     console.log('doble click');
+// });
+
+// div.addEventListener('mouseenter',()=>{
+//     div.classList.add('red');
+// });
+
+// div.addEventListener('mouseleave',()=>{
+//     div.classList.remove('red');
+
+// });
+div.addEventListener('mouseenter',()=>{
+        div.classList.add('red')
+});
+// button.addEventListener('mouseup',()=>{
+//     div.classList.remove('red');
+
+// });
+
+// div.addEventListener();
