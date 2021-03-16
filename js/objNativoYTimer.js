4e805d993bc01538bb2c3313cf158cdc52ddc4e1
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-03-16 17:46:00 -0600

    Sigue pendiente el video 31

--- js/objNativoYTimer.js
+++ js/objNativoYTimer.js
@@ -18,3 +18,31 @@ const person ={
 
 
 console.table(person);
+
+//OBJETO LOCATION
+console.log(location.href);
+console.log(location.protocol);
+console.log(location.host);
+console.log(location.pathname);
+console.log(location.hash);
+//CARGA LA PAGINA 
+//location.reload();
+//Redirige a otra pagina
+//location.href='https://google.com'
+
+//OBJETO HISTORY
+
+//historial de mi navegador en la sesion actual (si te sales del navegador ya no funciona)
+//BACK() HACIA ATRAS
+//history.back();
+//BACK() HACIA ADELANTE
+//history.forward();
+//BACK() NUMERO NEGATIVO O POSITIVO COMO PARAMETRO PARA NAVEGAR CIERTO NUMERO DE PAGINAS HACIA ATRAS O ADELANTE
+//history.go();
+
+//OBJETO DATE
+
+const date = new Date();
+
+//TIENE VARIOS METODOS PARA TRABAJAR CON EL TIEMPO
+console.log(date);
