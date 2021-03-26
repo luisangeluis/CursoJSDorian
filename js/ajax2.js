1c0636d17ff4ea0054070ea7212f5f7045191669
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-03-26 15:03:43 -0600

    Falta que muestre la info del usuario seleccionado

--- js/ajax2.js
+++ js/ajax2.js
@@ -1,5 +1,4 @@
-
-
+/*
 const selectUsers = document.querySelector('#select-users');
 
 
@@ -39,4 +38,99 @@ const selectUsers = document.querySelector('#select-users');
     xhr.send();
 
 //});
+*/
+const selectUsers = document.querySelector('#characters');
+const botonGetData = document.querySelector('#boton');
 
+const table = document.querySelector('#table');
+
+
+document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    getData(selectUsers.children[selectUsers.selectedIndex].value)
+})
+
+const getData = (id) => {
+
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+
+    if (id == undefined) {
+        xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+
+
+        xhr.addEventListener('load', (data) => {
+
+            const dataJSON = JSON.parse(data.target.response);
+            console.log(dataJSON);
+
+            const fragmen = document.createDocumentFragment();
+
+            dataJSON.forEach(user => {
+                const option = document.createElement('option');
+                option.setAttribute('value',user.id);
+                option.textContent = user.name;
+                fragmen.appendChild(option);
+
+
+            });
+
+            selectUsers.appendChild(fragmen);
+        });
+    } else {
+        /*
+        xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+
+        xhr.addEventListener('load', (data) => {
+
+            const dataJSON = JSON.parse(data.target.response);
+            const fragmen = document.createDocumentFragment();
+
+            botonGetData.addEventListener('click',()=>{
+
+                const tr = document.createElement('tr');
+                table.appendChild(tr);
+            });
+
+        });
+        */
+        xhr.open('GET', 'https://jsonplaceholder.typicode.com/users?id=${id}');
+
+        xhr.addEventListener('load', (data) => {
+
+            const dataJSON = JSON.parse(data.target.response);
+            const fragmen = document.createDocumentFragment();
+                           const tr = document.createElement('tr');
+
+            fragmen.appendChild(tr); 
+
+
+            dataJSON.forEach(user=>{
+               //const tr = document.createElement('tr');
+               const name = document.createElement('td');
+               const username = document.createElement('td');
+               const email = document.createElement('td');
+
+               name.textContent = user.name;
+               name.textContent = user.username;
+               name.textContent = user.email;
+
+               fragment.children[1].appendChil(name);
+               fragment.appendChil[1].appendChil(username);
+               fragment.appendChil[1].appendChil(email);
+
+            });
+            table.appendChild(fragmen);
+        });
+
+
+    }
+
+    xhr.send();
+}
+
+getData();
