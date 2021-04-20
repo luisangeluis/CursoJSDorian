c3dcb66b743ca558fa466f70610bb44f99a708e9
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-04-20 16:28:57 -0500

    video 40 async await medio terminado.

--- js/async-await.js
+++ js/async-await.js
@@ -1,6 +1,6 @@
 //alert('hola');
-
-const getName = async ()=>{
+/*
+const getName = /*async()=>{
 
     return new Promise((resolve,reject)=>{
 
@@ -10,6 +10,7 @@ const getName = async ()=>{
         },1500);
     });
 }
+*/
 /*
 const sayHello=()=>{
     const name=getName();
@@ -18,7 +19,7 @@ const sayHello=()=>{
 
 console.log(sayHello());
 */
-
+/*
 const sayHello= async ()=>{
     const name= await getName();
     return `hello ${name}`
@@ -27,4 +28,75 @@ const sayHello= async ()=>{
 sayHello()
     .then(res=>console.log(res));
 
-    
+*/
+
+const users = [
+    {
+        id: 1,
+        name: 'luis'
+    }, {
+        id: 2,
+        name: 'angel'
+    }, {
+        id: 3,
+        name: 'pedro'
+    }, {
+        id: 4,
+        name: 'pablo'
+    }
+]
+
+const emails = [
+    {
+        id: 1,
+        email: 'luis@correo'
+    },
+    {
+        id: 2,
+        email: 'angel@correo'
+    },
+    {
+        id: 3,
+        email: 'pedro@correo'
+    }
+]
+
+const getUser =  async(id) => {
+
+    const user = users.find(user => user.id == id);
+
+    if (!user) throw new Error('user no existe');
+    else return (user);
+
+}
+
+const getEmail =  async(user) => {
+
+    const email = emails.find(email => email.id == user.id);
+
+    if (!email) throw new Error('no hay email');
+
+    else return ({ user: user.id, name: user.name, email: email.email });
+
+}
+
+
+const getInfo = async (pId) =>{
+
+     getUser(pId).then(res=>console.log(res));
+
+    try {
+        const user = await getUser(pId)
+        const email = await getEmail(user);
+        return `Usuario: ${user.name} correo: ${email.email}`
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+getInfo(3)
+    .then(res => {
+        console.log(res);
+    }).catch(err=>console.log(err));
+
