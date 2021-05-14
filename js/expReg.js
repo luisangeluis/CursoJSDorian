d8634d5e71723dc6794ebe3f91102d6f671e24ac
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-05-14 16:48:03 -0500

    Video 46 expresiones regulares III terminado.

--- js/expReg.js
+++ js/expReg.js
@@ -24,7 +24,18 @@ const text = document.querySelector('#texto').textContent;
 // const expReg = /[a-d]/gi;
 
 //() los parentesis son para buscar una o mas cadenas completas, ¡¡¡es sensible a espacios en blanco!!!
-const expReg = /(lorem|sit)/gi;
+// const expReg = /(lorem|sit)/gi;
+
+// ^ acento circunflejo es para indicar que no debe de haber nada antes de este simbolo
+// $ Dolar es para indicar que no debe de haber nada despues del simbolo
+// const expReg = /^a$/gi;
+
+//Las llaves indican el numero de veces que se tiene que repetir lo que esta a la izquierda para que se true
+//se puede poner minimo {2} minimo con maximo {2,5} y minimo hasta infinito {2,}
+// const expReg = /^[a-zA-Z]{5}@{2}$/g
+//? Con este simbolo indicamos que puede o no estar pero si está solo seria una vez
+//+ Este indica que lo que esta a la izquierda debe estar 1 vez como minimo para que sea valido.
+const expReg = /.*@.*\.{1}.*/gi
 
 
 
