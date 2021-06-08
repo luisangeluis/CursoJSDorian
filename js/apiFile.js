f14c4efab1cf658c96faa63c7b0f36726e8b252d
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-06-08 17:32:08 -0500

    Video 52 api file terminado.

--- js/apiFile.js
+++ js/apiFile.js
@@ -2,18 +2,19 @@ const inputFile = document.querySelector('#input-file');
 const texto = document.querySelector('#text');
 const img = document.querySelector('#img');
 
+//CON ARCHIVO DE TEXTO
 // inputFile.addEventListener('change',(e)=>{
 //     console.log(e);    
 //     console.log(e.target.files[0]);
 //     //Se captura la info en una variable
 //     const file = e.target.files[0];
 //     console.log(file);
-    
+
 //     //Se crea una variable de tipo FileReader
 //     const fileReader = new FileReader();
 
 //     fileReader.readAsText(file);
-    
+
 //     //ReadAsText es asincrona, usar el evento load
 //     fileReader.addEventListener('load',(e)=>{
 //         console.dir(e);
@@ -24,20 +25,47 @@ const img = document.querySelector('#img');
 
 // });
 
-inputFile.addEventListener('change',(e)=>{
+//CON ARCHIVO DE IMAGEN
+// inputFile.addEventListener('change',(e)=>{
 
+//     console.log(e);
+//     const file = e.target.files[0];
+
+//     const fileReader = new FileReader();
+
+//     fileReader.readAsDataURL(file);
+
+//     fileReader.addEventListener('load',(e)=>{
+//         console.log(e);
+//         img.setAttribute('src',e.target.result);
+
+
+//     });
+// });
+
+//CON MULTIPLES IMAGENES
+//HAY QUE AGREGAR EL ATRIBUTO MULTIPLE
+inputFile.addEventListener('change', (e) => {
     console.log(e);
-    const file = e.target.files[0];
+    const files = e.target.files;
 
-    const fileReader = new FileReader();
+    const fragment = document.createDocumentFragment();
 
-    fileReader.readAsDataURL(file);
+    for(const element of files){
+        const fileReader = new FileReader();
+        const img = document.createElement('img');
 
-    fileReader.addEventListener('load',(e)=>{
-        console.log(e);
-        img.setAttribute('src',e.target.result);
+        fileReader.readAsDataURL(element);
 
+        fileReader.addEventListener('load', (e) => {
+            img.setAttribute('src', e.target.result);
+        });
 
-    });
-});
+        fragment.appendChild(img);
+    }
+    
+
+    document.getElementById('images').appendChild(fragment);
+
+})
 
