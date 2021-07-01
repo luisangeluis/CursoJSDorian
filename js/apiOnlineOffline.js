21b8db7e05c95d85d1b610ea1df9c575a8b91186
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-07-01 16:20:43 -0500

    Video 61 terminado api online y offline

---
+++ js/apiOnlineOffline.js
@@ -0,0 +1,10 @@
+const message = document.querySelector('#message');
+
+addEventListener('online',()=>{
+    console.log('online');
+});
+
+addEventListener('offline',()=>{
+    console.log('offline');
+});
+
